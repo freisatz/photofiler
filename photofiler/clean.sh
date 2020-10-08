8353e451c0eb8ab1457f8440e9a01df3bb7712a4
Robin <rst@mailbox.org>
2020-10-08 15:32:00 +0200

    Add app icon

--- photofiler/clean.sh
+++ photofiler/clean.sh
@@ -4,3 +4,4 @@ rm -rf /usr/local/lib/perl
 rm /usr/bin/exiftool
 rm /usr/bin/photofiler
 
+rm -rf /var/www/photofiler/
--- photofiler/init.sh
+++ photofiler/init.sh
@@ -9,3 +9,7 @@ ln -s $path/../Image-ExifTool-12.07/lib/File /usr/local/lib/perl/5.10.1/
 
 ln -s $path/../Image-ExifTool-12.07/exiftool /usr/bin/
 ln -s $path/bin/photofiler /usr/bin/
+
+mkdir -p /var/www/photofiler/
+
+ln -s $path/share/images/photofiler.png /var/www/photofiler/
