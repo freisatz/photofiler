9730c26a47829b5fb8f66e486170bc1418e1dab0
Robin <rst@mailbox.org>
2020-10-08 15:35:35 +0200

    Change location exiftool is extracted to

--- photofiler/init.sh
+++ photofiler/init.sh
@@ -4,10 +4,10 @@ path=$1
 
 mkdir -p /usr/local/lib/perl/5.10.1
 
-ln -s $path/../Image-ExifTool-12.07/lib/Image /usr/local/lib/perl/5.10.1/
-ln -s $path/../Image-ExifTool-12.07/lib/File /usr/local/lib/perl/5.10.1/
+ln -s $path/share/Image-ExifTool-12.07/lib/Image /usr/local/lib/perl/5.10.1/
+ln -s $path/share/Image-ExifTool-12.07/lib/File /usr/local/lib/perl/5.10.1/
 
-ln -s $path/../Image-ExifTool-12.07/exiftool /usr/bin/
+ln -s $path/share/Image-ExifTool-12.07/exiftool /usr/bin/
 ln -s $path/bin/photofiler /usr/bin/
 
 mkdir -p /var/www/photofiler/
--- photofiler/install.sh
+++ photofiler/install.sh
@@ -3,7 +3,7 @@
 path_src=$1
 path_des=$2
 
-tar xzvf $path_src/Image-ExifTool-12.07.tar.gz --directory=$path_des
+tar xzvf $path_src/Image-ExifTool-12.07.tar.gz --directory=$path_src/share/
 rm -f $path_src/Image-ExifTool-12.07.tar.gz
 
 mv $path_src $path_des
